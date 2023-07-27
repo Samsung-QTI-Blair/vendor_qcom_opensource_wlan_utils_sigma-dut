089d4b43e3fb79fbd32b69de3592cff0dbebb896
Shivani Baranwal <quic_shivbara@quicinc.com>
2023-07-27 15:58:59 +0530

    nan: Replace deprecated function sprintf() with snprintf()
    Replace the usage of banned function sprintf() with the alternative
    function snprintf().
    
    CRs-Fixed: 3568681
    Change-Id: I166212d289dc230817803f04dddd2682fb349695

--- nan.c
+++ nan.c
@@ -3550,8 +3550,7 @@ int nan_cmd_sta_get_parameter(struct sigma_dut *dut, struct sigma_conn *conn,
 	u32 sched_update_channel_freq;
 #endif
 #ifdef WFA_CERT_NANR4
-	int i;
-	char *temp;
+	int i, pos;
 	char string[100];
 	wifi_error ret;
 #endif /* WFA_CERT_NANR4 */
@@ -3666,9 +3665,14 @@ int nan_cmd_sta_get_parameter(struct sigma_dut *dut, struct sigma_conn *conn,
 			sigma_dut_print(dut, DUT_MSG_ERROR, "Request failed");
 			return -1;
 		}
-		temp = string;
-		for (i = 0; i < msg.tk_len; i++)
-			temp += sprintf(temp, "%02x", msg.tk[i]);
+		pos = 0;
+		for (i = 0; i < msg.tk_len; i++) {
+			ret = snprintf(&(string[pos]), sizeof(string) - pos,
+					"%02x", msg.tk[i]);
+			if (ret < 0 || ret >= (sizeof(string) - pos))
+				break;
+			pos += ret;
+		}
 		string[msg.tk_len * 2] = '\0';
 		snprintf(resp_buf, sizeof(resp_buf), "TK,%s", string);
 	} else if (strcasecmp(parameter, "PMKID") == 0) {
@@ -3687,9 +3691,14 @@ int nan_cmd_sta_get_parameter(struct sigma_dut *dut, struct sigma_conn *conn,
 			sigma_dut_print(dut, DUT_MSG_ERROR, "Request failed");
 			return -1;
 		}
-		temp = string;
-		for (i = 0; i < msg.pmkid_len; i++)
-			temp += sprintf(temp, "%02x", msg.pmkid[i]);
+		pos = 0;
+		for (i = 0; i < msg.pmkid_len; i++) {
+			ret = snprintf(&(string[pos]), sizeof(string) - pos,
+					"%02x", msg.pmkid[i]);
+			if (ret < 0 || ret >= (sizeof(string) - pos))
+				break;
+			pos += ret;
+		}
 		string[msg.pmkid_len * 2] = '\0';
 		snprintf(resp_buf, sizeof(resp_buf), "PMKID,%s", string);
 #endif /* WFA_CERT_NANR4 */
